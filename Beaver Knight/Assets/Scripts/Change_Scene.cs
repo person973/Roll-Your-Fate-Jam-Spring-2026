cf6d5ce216080d430a85c7569e47df7d38be3e5d
falicov <121999608+falicov@users.noreply.github.com>
2026-03-22 15:37:46 -0400

    squire, movable blocks, a few other things

--- Beaver Knight/Assets/Scripts/Change_Scene.cs
+++ Beaver Knight/Assets/Scripts/Change_Scene.cs
@@ -1,11 +1,18 @@
-using UnityEditor.SceneManagement;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Change_Scene : MonoBehaviour
 {
+    [SerializeField] string scene;
     public void NewScene(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.CompareTag("Player"))
+        {
+            NewScene(scene);
+        }
     }
 }
